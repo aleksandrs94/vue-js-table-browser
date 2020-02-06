e7229930f03209bbbcfdf00ba339469f4d6ad117
Aleksandrs Sprudzans <aleksandrs.sprudzans@gmail.com>
2020-02-06 10:24:11 +0200

    Clean Vue Project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TableView />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TableView from "./components/TableView.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    TableView
   }
 };
 </script>
